2dfd46056630fd199d268c74be7ee6fbb8ed1867
Gowrikrish@1712 <g.gowri1@tcs.com>
2023-09-23 15:25:50 +0000

    Files forfreestyle jenkins

---
+++ new/my-app/src/main/java/com/mycompany/app/App.java
@@ -0,0 +1,15 @@
+package com.mycompany.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+ int a =20, b=10,c=30;
+    public static void main( String[] args )
+    {
+       int d = a+b+c;
+        System.out.println( "Weclome to branch3"+d );
+    }
+}
